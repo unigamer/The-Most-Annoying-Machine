1467e70a8bb48f58402b58a1f8b87898d5d8b56d
unigamer <unigamer@gmail.com>
2015-03-16 21:56:31 +0000

    Tidied code
    Version before adding full sleep. Seems to be working fine.

--- Code/The_Most_Annoying_Machine/The_Most_Annoying_Machine.ino
+++ Code/The_Most_Annoying_Machine/The_Most_Annoying_Machine.ino
@@ -5,7 +5,17 @@
 // This is the first version of the code. I knocked it up in an evening and it might (read WILL) be buggy. Oh, and there are few comments and the ones that do exist are probably wrong.
 
 
-const int buttonPin = 3;     // the number of the pushbutton pinconst int buttonPin = 2;     // the number of the pushbutton pin
+/*
+Todo:
+- comment code
+- go into sleep when "off" mode is activated, maybe use an interrupt
+- schematic for this code
+- remove hard coded delays and change to variables
+- a general tidy up
+*/
+
+#include <math.h>
+
 int buttonState = 0;         // variable for reading the pushbutton status
 
 int accel_flag = 0;
@@ -13,20 +23,17 @@ int in_rest = 0;
 unsigned long time;
 unsigned long  button_switch_time ;
 unsigned long  accel_time ;
-  unsigned long button_delay ; 
-
+unsigned long button_delay ; 
+double magnitude_accelerometer;
 int previous_button_state = 1;
 
 const int ledPin =  8; 
 const int buzzerPin =  7; 
 const int activateswitchPin =  4; 
-const int sirenPin =  10; 
- 
-#include <math.h>
-
-int xpin;             
-int ypin;                 
-int zpin;
+const int buttonPin = 3;     // the number of the pushbutton pinconst int buttonPin = 2;     // the number of the pushbutton pin
+const int  xpin = A0;
+const int  ypin = A1;
+const int  zpin = A2;
 
 int state = 1; // 1 = active, 0, inactive where active means the thing is trying to flip the switch;
 
@@ -34,62 +41,41 @@ int x; // Acceleration in the x-direction
 int y; // Acceleration in the y-direction
 int z; // Acceleration in the z-direction
 
-double magnitude_accelerometer;
-
 // These values are used to scale the acceleration that is read from the accelerometer. This is required because the voltage varies as the Lipo drains down.
-int xMin;
-int yMin;
-int zMin;
-int xMax;
-int yMax;
-int zMax;
-
-  
+    // Calibrate the accelerometer
+const int   xMin = 270; 
+const int   yMin = 265;
+const int   zMin = 282;
+const int   xMax = 405;
+const int   yMax = 400;
+const int   zMax = 416;
 
 void setup(void) {
     pinMode(buttonPin, INPUT);  
     pinMode(buzzerPin, OUTPUT);  
     pinMode(activateswitchPin, OUTPUT);
     pinMode(ledPin, OUTPUT);
-    pinMode(sirenPin, OUTPUT);
-    xpin = A0;
-    ypin = A1;
-    zpin = A2;
-    
-    xMin = 270; 
-    yMin = 265;
-    zMin = 282;
-    xMax = 405;
-    yMax = 400;
-    zMax = 416;
-
     pinMode(xpin, INPUT);
     pinMode(ypin, INPUT);
     pinMode(zpin, INPUT);
-    Serial.begin(57600);
-  
+
+    Serial.begin(57600); 
  }
  
  void loop(void)
 {
 
   buttonState = digitalRead(buttonPin);
-  x = analogRead(xpin); 
-  delay(1); 
-  y = analogRead(ypin); 
-  delay(1); 
-  z = analogRead(zpin);
-  
+  x = analogRead(xpin); delay(1); y = analogRead(ypin); delay(1); z = analogRead(zpin);
   
   if (buttonState == HIGH) {    
     // turn LED on:    
     if (previous_button_state==0) {      
       button_switch_time = millis();
-      Serial.println("meow");
     }
    digitalWrite(buzzerPin, HIGH);    
  digitalWrite(ledPin, HIGH);   
- digitalWrite(sirenPin, HIGH);   
+  
    previous_button_state = 1;
    in_rest = 0;
   
@@ -99,8 +85,6 @@ void setup(void) {
     if (in_rest != 1) {
     
     if (previous_button_state == 1) {      
-   
-  
   button_delay = (millis()-button_switch_time);    
       button_delay = min((button_delay),3000);   
    
@@ -112,36 +96,25 @@ void setup(void) {
     }
     
      previous_button_state = 0;
-    // turn LED off:
     digitalWrite(buzzerPin, LOW);
-    digitalWrite(sirenPin, LOW);
     digitalWrite(ledPin, LOW);
     
     if (millis()>time) {
      digitalWrite(activateswitchPin, HIGH); 
      delay(30);
      digitalWrite(activateswitchPin, LOW); 
-     delay(200);
-    
+     delay(200);   
     }
     
     }
   }
-  
  
-  
-  
-    int xAng = map(x, xMin, xMax, -90, 90);
+  int xAng = map(x, xMin, xMax, -90, 90);
   int yAng = map(y, yMin, yMax, -90, 90);
   int zAng = map(z, zMin, zMax, -90, 90);
 
   magnitude_accelerometer = sqrt(pow(xAng,2)+pow(yAng,2)+pow(zAng,2)); // Determine the magnitude of the acceleration
   
-  
-// Serial.println(magnitude_accelerometer);
- //delay(10);
- 
- 
  if (millis()>accel_time && accel_flag==1)
   if (magnitude_accelerometer<40) {
   
@@ -159,9 +132,7 @@ void setup(void) {
     accel_time = millis()+300;
     accel_flag = 1;  
   }
-  
-
-  
+    
 }
 
 
